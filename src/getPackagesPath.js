f05e000721d46d7c4c08516a5dff4a5cec1abbd7
jalal246 <jimmy002020@gmail.com>
2020-02-13 18:50:18 +0200

    Add test to getPackagesPath. Add new functionality to filter path and validate.

--- src/getPackagesPath.js
+++ src/getPackagesPath.js
@@ -1,11 +1,35 @@
 const fs = require("fs");
 const glob = require("glob");
+const { resolve } = require("path");
 
-const { msg, success, error } = require("@mytools/print");
+const { msg, success, error, warning } = require("@mytools/print");
+
+/**
+ * Validate `package.json` & `src` for given path.
+ *
+ * @param {string} dir
+ * @returns {boolean} true if valid
+ */
+function validateAccessability(dir) {
+  const pkgJson = resolve(dir, "package.json");
+  const src = resolve(dir, "src");
+
+  try {
+    fs.accessSync(pkgJson, fs.constants.R_OK);
+    fs.accessSync(src, fs.constants.R_OK);
+  } catch (e) {
+    warning(e);
+
+    return false;
+  }
+
+  return true;
+}
 
 /**
  *
- * Gets packages path for a given project source root.
+ * Gets packages path for a given project source root. It filters each path
+ * using `validateAccessability`
  *
  * @param {Object} input
  * @param {string} input.dir  packages path [path="./packages/*"]
@@ -22,13 +46,13 @@ function getPackagesPath({ dir = "./packages/*" } = {}) {
    * If length is zero, not monorepo.
    */
   if (folders.length === 0) {
-    try {
-      fs.accessSync("./src", fs.constants.R_OK);
-    } catch (e) {
-      error(e);
+    if (validateAccessability(".")) {
+      folders.push(".");
+    } else {
+      error("Unable to read package form project root directory");
     }
-
-    folders.push(".");
+  } else {
+    folders = folders.filter(pkgDir => validateAccessability(pkgDir));
   }
 
   success(`> Found ${folders.length} packages`);
---
+++ test/getPackagesPath.test.js
@@ -0,0 +1,29 @@
+const { expect } = require("chai");
+
+const { getPackagesPath, setIsSilent } = require("../src");
+
+setIsSilent(true);
+
+describe("testing getPackagesPath()", () => {
+  it("returns array contains paths for all-valid packages", () => {
+    const packagesPath = getPackagesPath({ dir: "./test/packages-valid/*" });
+
+    const expected = [
+      "./test/packages-valid/folo-forms",
+      "./test/packages-valid/folo-layout",
+      "./test/packages-valid/folo-utils",
+      "./test/packages-valid/folo-values",
+      "./test/packages-valid/folo-withcontext"
+    ];
+
+    expect(packagesPath).to.deep.equal(expected);
+  });
+
+  it("returns filtered array contains only valid packages", () => {
+    const packagesPath = getPackagesPath({ dir: "./test/packages-invalid/*" });
+
+    const expected = ["./test/packages-invalid/folo-forms"];
+
+    expect(packagesPath).to.deep.equal(expected);
+  });
+});
