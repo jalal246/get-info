893f4592b2f0230f9a552c1a898eeb20c61deb28
jalal246 <jimmy002020@gmail.com>
2020-02-28 17:42:25 +0200

    fix tests

--- test/getJsonByName.test.js
+++ test/getJsonByName.test.js
@@ -1,21 +1,20 @@
 const { expect } = require("chai");
-const { getJsonByName, getPackagesPath, setIsSilent } = require("../src");
+const { getJsonByName, setIsSilent } = require("../src");
 
 describe("getJsonByName", () => {
   setIsSilent(true);
   it("Default: gets current path with ext and json", () => {
-    const { ext, json, path } = getJsonByName()();
+    const { ext, json, distPath } = getJsonByName()();
 
     expect(json).to.be.an("Array");
     expect(ext).to.be.an("Array");
-    expect(path).to.be.an("Array");
+    expect(distPath).to.be.an("Array");
 
     expect(json.length).to.be.equal(1);
     expect(ext.length).to.be.equal(1);
-    expect(path.length).to.be.equal(1);
+    expect(distPath.length).to.be.equal(1);
 
     expect(ext[0]).to.be.equal("js");
-    expect(path[0]).to.be.equal(".");
     expect(json[0]).to.have.own.property("sourcePath");
     expect(json[0]).to.have.own.property("dependencies");
 
@@ -24,17 +23,6 @@ describe("getJsonByName", () => {
      */
     expect(json[0].name).to.be.equal("get-info");
   });
-
-  // TODO
-  // it("TODO: gets all packages for specific path", () => {
-  //   const { path } = getPackagesPath({
-  //     dir: "./test/packages-valid/*"
-  //   });
-
-  //   const { ext, json } = getJsonByName({
-  //     path
-  //   })();
-  // });
 
   it("gets array of json with default path", () => {
     const { json } = getJsonByName()("get-info");
@@ -50,20 +38,5 @@ describe("getJsonByName", () => {
 
     expect(json).to.be.an("Array");
     expect(json.length).to.be.equal(0);
-  });
-
-  it("gets array of path with ext and json for given path", () => {
-    const { path, ext } = getPackagesPath("./test/packages-valid/*");
-
-    const { json } = getJsonByName({
-      path,
-      ext
-    })("folo/form", "folo/values");
-
-    expect(json).to.be.an("Array");
-    expect(json.length).to.be.equal(2);
-
-    expect(json[0].name).to.be.equal("@folo/forms");
-    expect(json[1].name).to.be.equal("@folo/values");
   });
 });
