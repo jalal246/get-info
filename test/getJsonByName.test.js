a9338f003daf1cafcaca8907dfeb3d5140d2fc4e
jalal246 <jimmy002020@gmail.com>
2020-03-30 20:15:23 +0200

    Add fixes to test

--- test/getJsonByName.test.js
+++ test/getJsonByName.test.js
@@ -1,33 +1,35 @@
 const { expect } = require("chai");
-const { getJsonByName, getPackagesPath, setIsSilent } = require("../src");
+const { getJsonByName, setIsSilent } = require("../src");
 
 describe("getJsonByName", () => {
   setIsSilent(true);
+
   it("Default: gets current path with ext and json", () => {
-    const { json, pkgInfo } = getJsonByName()();
+    const { json, pkgInfo } = getJsonByName();
 
     expect(json).to.be.an("Array");
     expect(pkgInfo).to.be.an("Object");
 
     expect(json.length).to.be.equal(1);
 
-    expect(json[0]).to.have.own.property("sourcePath");
     expect(json[0]).to.have.own.property("dependencies");
 
     /**
      * By default, will read the project scr and package json.
      */
-    expect(json[0].name).to.be.equal("get-info");
-    const { name } = json[0];
+    const { name, dependencies } = json[0];
 
-    const { dist, ext } = pkgInfo[name];
+    expect(name).to.be.equal("get-info");
+    expect(dependencies).to.be.an("Object");
 
-    expect(dist).to.be.an("string");
+    const { srcPath, ext } = pkgInfo[name];
+
+    expect(srcPath).to.be.an("string");
     expect(ext).to.be.equal("js");
   });
 
   it("gets array of json with default path", () => {
-    const { json } = getJsonByName()("get-info");
+    const { json } = getJsonByName("get-info");
 
     expect(json).to.be.an("Array");
     expect(json.length).to.be.equal(1);
@@ -35,33 +37,33 @@ describe("getJsonByName", () => {
     expect(json[0].name).to.be.equal("get-info");
   });
 
-  it("gets array of json with a given path", () => {
-    const { path } = getPackagesPath("./test/packages-valid/*");
+  // it("gets array of json with a given path", () => {
+  //   const { path } = getPackagesPath("./test/packages-valid/*");
 
-    const { json } = getJsonByName("dist", ...path)();
+  //   const { json } = getJsonByName("dist", ...path)();
 
-    expect(json).to.be.an("Array");
-    expect(json.length).to.be.equal(5);
+  //   expect(json).to.be.an("Array");
+  //   expect(json.length).to.be.equal(5);
 
-    expect(json[0].name).to.be.equal("@folo/forms");
-    expect(json[1].name).to.be.equal("@folo/layout");
-    expect(json[3].name).to.be.equal("@folo/values");
-    expect(json[4].name).to.be.equal("@folo/withcontext");
-  });
+  //   expect(json[0].name).to.be.equal("@folo/forms");
+  //   expect(json[1].name).to.be.equal("@folo/layout");
+  //   expect(json[3].name).to.be.equal("@folo/values");
+  //   expect(json[4].name).to.be.equal("@folo/withcontext");
+  // });
 
-  it("gets array of json with a given path and names", () => {
-    const { path } = getPackagesPath("./test/packages-valid/*");
+  // it("gets array of json with a given path and names", () => {
+  //   const { path } = getPackagesPath("./test/packages-valid/*");
 
-    const { json } = getJsonByName("dist", ...path)("@folo/forms");
+  //   const { json } = getJsonByName("dist", ...path)("@folo/forms");
 
-    expect(json).to.be.an("Array");
-    expect(json.length).to.be.equal(1);
+  //   expect(json).to.be.an("Array");
+  //   expect(json.length).to.be.equal(1);
 
-    expect(json[0].name).to.be.equal("@folo/forms");
-  });
+  //   expect(json[0].name).to.be.equal("@folo/forms");
+  // });
 
   it("returns empty array when name is wrong", () => {
-    const { json } = getJsonByName()("nothingTrue");
+    const { json } = getJsonByName("nothingTrue");
 
     expect(json).to.be.an("Array");
     expect(json.length).to.be.equal(0);
